7ec0f3cdc0fa914c3f40c13a95778d15ae3d5f18
SS-yuga-ehara <SS-yuga-ehara@smart-group.co.jp>
2026-06-29 11:33:52 +0900

    :feat: first commit

---
+++ src/test/java/com/example/protospace_c/ProtospaceCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.protospace_c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtospaceCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
